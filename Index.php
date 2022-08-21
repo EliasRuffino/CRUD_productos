871256693d796cd52a2d4c87c340e4f46291127f
Elias Ruffino <eliasruffino@gmail.com>
2022-08-21 16:04:45 -0300

    Cargue Productos

--- Index.php
+++ Index.php
@@ -9,10 +9,13 @@ $pdo ->setAttribute(\PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $consulta = $pdo -> prepare ("SELECT * FROM productos ORDER BY fecha_creacion");
 $consulta->execute();
 $productos = $consulta->fetchAll(PDO::FETCH_ASSOC);
-echo "<pre>";
+/*echo "<pre>";
 var_dump($productos);
 echo "</pre>";
+*/
+
 ?>
+
 
 <!doctype html>
 <html lang="en">
@@ -29,29 +32,28 @@ echo "</pre>";
   <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Imagen</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Descripcion</th>
+      <th scope="col">Precio</th>
+      <th scope="col">Fecha de Creacion</th>
+      
+
     </tr>
   </thead>
   <tbody>
+    <?php foreach ($productos as $i => $producto){ ?>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row"><?=$i+1?></th>
+      <td></td>
+      <td><?=$producto['nombre']?></td>
+      <td><?=$producto['descripcion']?></td>
+      <td><?=$producto['precio']?></td>
+      <td><?=$producto['fecha_creacion']?></td>
+      
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+    <?php  } ?>
+
   </tbody>
 </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
