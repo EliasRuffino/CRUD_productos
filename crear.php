7c51ed3bb8c14693c02aa62a8ad2858a8dd33595
Elias Ruffino <eliasruffino@gmail.com>
2022-08-21 16:55:30 -0300

    agregamos formulario

--- crear.php
+++ crear.php
@@ -22,47 +22,39 @@ echo "</pre>";
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Mis Productos</title>
+    <title>Creacion de Productos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css" rel="stylesheet" type="text/css">
 </head>
   <body>
-    <h1>CRUD DE PRODUCTOS</h1>
-    <a href="./crear.php"><button type="button" class="btn btn-outline-success " btn-lg>Crear Producto</button>
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Imagen</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Descripcion</th>
-      <th scope="col">Precio</th>
-      <th scope="col">Fecha de Creacion</th>
-      
-      
+    <h1>CREAR PRODUCTO</h1>
+    <a href="index.php"><button type="button" class="btn btn-outline-success " btn-lg>Volver</button>
+    
+    <form>
+  <div class="mb-3">
+    <label>Imagen</label>
+    <input type="file" class="form-control" id="imagen" name = "imagen">
+  </div>
+  <div class="mb-3">
+    <label>Nombre</label>
+    <input type="text" class="form-control" id="nombre" name = "nombre" require>
+    </div>
+    <div class="mb-3">
+    <label>Descripcion</label>
+    <textarea class="form-control" id="descripcion" name = "descripcion"></textarea >
+  </div> 
+  <div class="mb-3">
+    <label>Precio</label>
+    <input type="number" step=".01" class="form-control" id="precio" name = "precio" require>
+  </div>
+  <div class="mb-3">
+    <label>Fecha de Creacion</label>
+    <input type="date" class="form-control" id="fecha" name = "fecha">
+  </div>
 
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($productos as $i => $producto){ ?>
-    <tr>
-      <th scope="row"><?=$i+1?></th>
-      <td></td>
-      <td><?=$producto['nombre']?></td>
-      <td><?=$producto['descripcion']?></td>
-      <td><?=$producto['precio']?></td>
-      <td><?=$producto['fecha_creacion']?></td>
-      <td>
-        <button type="button" class="btn btn-info" btn-sm>Editar</button>
-        <button type="button" class="btn btn-danger" btn-sm>Borrar</button>
+  <button type="submit" class="btn btn-primary">Cargar Producto</button>
+</form>
 
-      </td>
-      
-    </tr>
-    <?php  } ?>
-
-  </tbody>
-</table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
